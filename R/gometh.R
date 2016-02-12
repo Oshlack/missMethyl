0296db78f3121d994bc948cc88189138dae7de4b
Belinda Phipson <belinda.phipson@mcri.edu.au>
2016-02-12 03:35:50 +0000

    Added new functions for performing gene set testing with user specified lists of gene sets. Added kegg pathway analysis to gometh.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/missMethyl@113553 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/gometh.R
+++ R/gometh.R
@@ -1,15 +1,72 @@
-gometh <- function(sig.cpg, all.cpg=NULL, plot.bias=FALSE, prior.prob=TRUE)
-# Gene ontology testing for 450K methylation arrays based on goseq
+gometh <- function(sig.cpg, all.cpg=NULL, collection="GO", plot.bias=FALSE, prior.prob=TRUE)
+# Gene ontology testing or KEGG pathway analysis for 450K methylation arrays based on goseq
 # Takes into account probability of differential methylation based on
 # numbers of probes on array per gene
 # Belinda Phipson
-# 28 January 2015
+# 28 January 2015. Last updated 10 February 2016.
 {
-    # check input
-    sig.cpg <- as.character(sig.cpg)
-    sig.cpg <- sig.cpg[!is.na(sig.cpg)]
+    if(!is.vector(sig.cpg))
+        stop("Input CpG list is not a character vector")
+    # Get mapped entrez gene IDs from CpG probe names
+    out <- getMappedEntrezIDs(sig.cpg=sig.cpg,all.cpg=all.cpg)
+    sorted.eg.sig <- out$sig.eg
+    eg.universe <- out$universe
+    freq_genes <- out$freq
+    test.de <- out$de
     
-    # flatten 450k array annotation
+    # get gene-wise prior probabilities and perform testing
+    if(prior.prob){
+        pwf <- .estimatePWF(D=test.de,bias=as.vector(freq_genes))
+        if(plot.bias)
+            .plotBias(D=test.de,bias=as.vector(freq_genes))
+        if(collection=="GO")   
+            gst <- goana(sorted.eg.sig,universe=eg.universe,prior.prob=pwf)
+        if(collection=="KEGG")
+            gst <- kegga(sorted.eg.sig,universe=eg.universe,prior.prob=pwf)
+        #    stop("KEGG with prior probability not available at the moment. Please run gometh with prior.prob=FALSE for KEGG pathway analysis.")
+    }
+    # Perform GO or KEGG testing without correcting for CpG density bias
+    else{
+        if(collection=="GO")
+            gst <- goana(sorted.eg.sig,universe=eg.universe)
+        if(collection=="KEGG")
+            gst <- kegga(sorted.eg.sig,universe=eg.universe)
+    }
+    gst$FDR<-p.adjust(gst$P.DE,method="BH")
+    gst
+}
+
+.plotBias <- function(D,bias)
+# Plotting function to show gene level CpG density bias
+# Belinda Phipson
+# 5 March 2015
+{
+    o <- order(bias)
+    splitf <- rep(1:100,each=200)[1:length(bias)]
+    avgbias <- tapply(bias[o],factor(splitf),mean)
+    sumDM <- tapply(D[o],factor(splitf),sum)
+    propDM <- sumDM/table(splitf)
+    par(mar=c(5,5,2,2))
+    plot(avgbias,as.vector(propDM),xlab="Number of CpGs per gene (binned)", ylab="Proportion Differential Methylation",cex.lab=1.5,cex.axis=1.2)
+    lines(lowess(avgbias,propDM),col=4,lwd=2)
+}
+
+.estimatePWF <- function(D,bias)
+# An alternative to goseq function nullp, which is transformation invariant
+# Belinda Phipson and Gordon Smyth
+# 6 March 2015
+{
+    prior.prob <- bias
+    o <- order(bias)
+    prior.prob[o] <- tricubeMovingAverage(D[o],span=0.5)
+    prior.prob
+}
+
+.flattenAnn <- function()
+# flatten 450k array annotation
+# Belinda Phipson
+# 10 February 2016
+{
     ann.450k = getAnnotation(IlluminaHumanMethylation450kanno.ilmn12.hg19)
     
     # get rid of the non-CpG sites
@@ -44,6 +101,20 @@ gometh <- function(sig.cpg, all.cpg=NULL, plot.bias=FALSE, prior.prob=TRUE)
     id<-paste(flat$cpg,flat$entrezid,sep=".")
     d <- duplicated(id)
     flat.u <- flat[!d,]
+    flat.u
+}
+
+getMappedEntrezIDs <- function(sig.cpg,all.cpg=NULL)
+# From a list of CpG sites, obtain the Entrez Gene IDs that are used for testing pathway enrichment
+# Belinda Phipson
+# 10 February 2016
+{
+    # check input
+    sig.cpg <- as.character(sig.cpg)
+    sig.cpg <- sig.cpg[!is.na(sig.cpg)]
+    
+    # Get annotaton in appropriate format
+    flat.u <- .flattenAnn()
     
     if(is.null(all.cpg))
         all.cpg <- unique(flat.u$cpg)
@@ -68,44 +139,6 @@ gometh <- function(sig.cpg, all.cpg=NULL, plot.bias=FALSE, prior.prob=TRUE)
     test.de <- as.integer(eg.universe %in% eg.sig)
     
     sorted.eg.sig <- eg.universe[test.de==1]
-    
-    # get gene-wise prior probabilities
-    if(prior.prob){
-        pwf <- .estimatePWF(D=test.de,bias=as.vector(freq_genes))
-        if(plot.bias)
-        	.plotBias(D=test.de,bias=as.vector(freq_genes))
-    # go testing
-        gst.go <- goana(sorted.eg.sig,universe=eg.universe,prior.prob=pwf)
-    }
-    else
-        gst.go <- goana(sorted.eg.sig,universe=eg.universe)
-    
-    gst.go$FDR<-p.adjust(gst.go$P.DE,method="BH")
-    gst.go
-}
-
-.plotBias <- function(D,bias)
-# Plotting function to show gene level CpG density bias
-# Belinda Phipson
-# 5 March 2015
-{
-    o <- order(bias)
-    splitf <- rep(1:100,each=200)[1:length(bias)]
-    avgbias <- tapply(bias[o],factor(splitf),mean)
-    sumDM <- tapply(D[o],factor(splitf),sum)
-    propDM <- sumDM/table(splitf)
-    par(mar=c(5,5,2,2))
-    plot(avgbias,as.vector(propDM),xlab="Number of CpGs per gene", ylab="Proportion Differential Methylation",cex.lab=1.5,cex.axis=1.2)
-    lines(lowess(avgbias,propDM),col=4,lwd=2)
-}
-
-.estimatePWF <- function(D,bias)
-# An alternative to goseq function nullp, which is transformation invariant
-# Belinda Phipson and Gordon Smyth
-# 6 March 2015
-{
-    prior.prob <- bias
-    o <- order(bias)
-    prior.prob[o] <- tricubeMovingAverage(D[o],span=0.5)
-    prior.prob
+    out <- list(sig.eg=sorted.eg.sig, universe=eg.universe, freq=freq_genes, de=test.de)
+    out
 }
---
+++ R/gsameth.R
@@ -0,0 +1,76 @@
+gsameth <- function(sig.cpg, all.cpg=NULL, collection, plot.bias=FALSE, prior.prob=TRUE)
+# Generalised version of gometh with user-specified gene sets 
+# Gene sets collections must be Entrez Gene ID
+# Can take into account probability of differential methylation 
+# based on numbers of probes on array per gene.
+# Belinda Phipson
+# 10 February 2016
+{
+    
+    if(!is.vector(sig.cpg))
+        stop("Input CpG list is not a character vector")
+    # Get mapped entrez gene IDs from CpG probe names
+    out <- getMappedEntrezIDs(sig.cpg=sig.cpg,all.cpg=all.cpg)
+    sorted.eg.sig <- out$sig.eg
+    eg.universe <- out$universe
+    freq_genes <- out$freq
+    test.de <- out$de
+    
+    # Check collection is a list with character vectors
+    if(!is.list(collection))
+            collection <- list(collection=collection)
+    collection <- lapply(collection, as.character)
+    # Make sure gene set collections don't have any NAs
+    collection <- lapply(collection, function(x) x[!is.na(x)])
+
+    # Estimate prior probabilities
+    if(prior.prob){
+        pwf <- .estimatePWF(D=test.de,bias=as.vector(freq_genes))
+        if(plot.bias)
+            .plotBias(D=test.de,bias=as.vector(freq_genes))
+    }
+        
+    results <- matrix(NA,ncol=4,nrow=length(collection))
+    colnames(results) <- c("N","DE","P.DE","FDR")
+    rownames(results) <- names(collection)
+    results[,"N"] <- unlist(lapply(collection,length))
+    results[,"DE"] <- unlist(lapply(collection, function(x) sum(sorted.eg.sig %in% x)))
+    Nuniverse <- length(eg.universe)
+    m <- length(sorted.eg.sig)
+        
+    # Hypergeometric test with prior probabilities
+    if(prior.prob){
+        for(i in 1:length(collection)){
+            InSet <- eg.universe %in% collection[[i]]
+            pw.red <- sum(pwf[InSet])/results[i,"N"]
+            pw.white <- sum(pwf[!InSet])/(Nuniverse-results[i,"N"])
+            odds <- pw.red/pw.white
+            results[i,"P.DE"] <- BiasedUrn::pWNCHypergeo(results[i,"DE"],results[i,"N"],Nuniverse-results[i,"N"],m,odds,lower.tail=FALSE)
+        }
+    }
+    # Hypergeometric test without prior probabilities
+    else{
+        for(i in 1:length(collection)){
+            results[i,"P.DE"] <- phyper(q=results[i,"DE"],m=m,n=Nuniverse-m,k=results[i,"N"],lower.tail=FALSE)    
+        }
+    }
+    results[,"FDR"] <- p.adjust(results[,"P.DE"],method="BH")
+    results
+}
+
+topGSA <- function(gsa,number=20,sort=TRUE)
+# Function to output the top 20 enriched gene sets from a gene set 
+# enrichment analysis using gsameth.
+# Belinda Phipson
+# 12 February
+{
+    if(nrow(gsa) < number)
+        number <- nrow(gsa)
+    if(sort){
+        o <- order(gsa[,"P.DE"])
+        out <- gsa[o,]
+        out[1:number,]
+    }
+    else
+        gsa[1:number,]
+}
