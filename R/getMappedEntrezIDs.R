3e13e7e4b8c3c5dc54d624e23a02674c3a2b8c6a
CalandraGrima <calandragrima@gmail.com>
2025-02-21 16:37:19 +1100

    bug fix: correct cpg naming for EPIC v2 and subsequent calculation of multimapping genes

--- R/getMappedEntrezIDs.R
+++ R/getMappedEntrezIDs.R
@@ -139,11 +139,7 @@ getMappedEntrezIDs <- function(sig.cpg, all.cpg=NULL,
     }
     
     if(is.null(all.cpg)) {
-      if (array.type == "EPIC_V2") {
-        all.cpg <- unique(rownames(flat.u))
-      } else {
         all.cpg <- unique(flat.u$cpg)
-      }
     } else {
         all.cpg <- as.character(all.cpg)
         all.cpg <- all.cpg[!is.na(all.cpg)]
@@ -151,21 +147,13 @@ getMappedEntrezIDs <- function(sig.cpg, all.cpg=NULL,
     }
     
     # remove CpGs from annotation that are not in all.cpg
-    if (array.type == "EPIC_V2") {
-      m_all <- match(rownames(flat.u), all.cpg)
-    } else {
-      m_all <- match(flat.u$cpg, all.cpg)
-    }
+    m_all <- match(flat.u$cpg, all.cpg)
     flat.u = flat.u[!is.na(m_all),]
     
     # map CpG sites to entrez gene id's
     sig.cpg <- unique(sig.cpg)
     
-    if (array.type == "EPIC_V2") {
-      m1 <- match(rownames(flat.u),sig.cpg)
-    } else {
-      m1 <- match(flat.u$cpg,sig.cpg)
-    }
+    m1 <- match(flat.u$cpg,sig.cpg)
     
     #eg.sig <- flat.u$entrezid[!is.na(m1)]
     if(any(grepl("ALL", genomic.features))){
@@ -180,11 +168,7 @@ getMappedEntrezIDs <- function(sig.cpg, all.cpg=NULL,
         stop("There are no genes annotated to the significant CpGs")
     }
     
-    if (array.type == "EPIC_V2") {
-      m2 <- match(rownames(flat.u),all.cpg)
-    } else {
-      m2 <- match(flat.u$cpg,all.cpg)
-    }
+    m2 <- match(flat.u$cpg,all.cpg)
     
     eg.all <- flat.u$entrezid[!is.na(m2)]
     
